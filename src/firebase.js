80a95a3a4ce5f21789367cb5355594f33ab35e0a
Aryan Dhakad <aryandhakad1@gmail.com>
2021-09-30 15:29:20 +0530

    changed firebase credentials

--- src/firebase.js
+++ src/firebase.js
@@ -3,24 +3,15 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/firestore";
 
-// const app = firebase.initializeApp({
-//   apiKey: "AIzaSyC8Bk_srQF6-cpdews7_OPhE9lIYmjMqvY",
-//   authDomain: "project5-e2264.firebaseapp.com",
-//   databaseURL: "https://project5-e2264-default-rtdb.firebaseio.com",
-//   projectId: "project5-e2264",
-//   storageBucket: "project5-e2264.appspot.com",
-//   messagingSenderId: "424059128465",
-//   appId: "1:424059128465:web:595fad9553ae37b3854d30",
-//   measurementId: "G-R5ERT7WVB0",
-// });
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBsOeJEFJhEbExjc-OUqzWPPwkBBm60Kp8",
-  authDomain: "learningfirestorequizzy.firebaseapp.com",
-  projectId: "learningfirestorequizzy",
-  storageBucket: "learningfirestorequizzy.appspot.com",
-  messagingSenderId: "880407213092",
-  appId: "1:880407213092:web:3b13e8fd2c44e06e8994b1",
-  measurementId: "G-GCZRB9LC8E"
+  apiKey: "AIzaSyDs6tyOFInCjMaDWuSrtZeLiyZe7nTNyZ4",
+  authDomain: "semv-3ba00.firebaseapp.com",
+  databaseURL: "https://semv-3ba00-default-rtdb.firebaseio.com",
+  projectId: "semv-3ba00",
+  storageBucket: "semv-3ba00.appspot.com",
+  messagingSenderId: "764651983056",
+  appId: "1:764651983056:web:5a1010393f29e17e788ed5",
+  measurementId: "G-SXX6Z94MN0",
 });
 
 export const auth = app.auth();
