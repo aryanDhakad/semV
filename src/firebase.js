a60812b690742584977ea391b2b9b16f7667bb9c
aryanDhakad <67039949+aryanDhakad@users.noreply.github.com>
2022-07-09 00:44:40 +0530

    Added .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,22 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "dotenv/config";
+
+const { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_DATABASE_URL, REACT_APP_PROJECT_ID,
+  REACT_APP_MEASUREMENT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID } = process.env;
+
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDs6tyOFInCjMaDWuSrtZeLiyZe7nTNyZ4",
-  authDomain: "semv-3ba00.firebaseapp.com",
-  databaseURL: "https://semv-3ba00-default-rtdb.firebaseio.com",
-  projectId: "semv-3ba00",
-  storageBucket: "semv-3ba00.appspot.com",
-  messagingSenderId: "764651983056",
-  appId: "1:764651983056:web:5a1010393f29e17e788ed5",
-  measurementId: "G-SXX6Z94MN0",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
+
 });
 
 export const auth = app.auth();
